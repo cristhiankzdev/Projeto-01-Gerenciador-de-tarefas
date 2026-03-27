c559bd988ede35dae0a9327090bd2c9dc4b9da27
Cristhiankzdev <cristhiankzdev@gmail.com>
2026-03-27 11:10:42 -0300

    config: chave anon JWT do Supabase corrigida

--- dayflow/js/supabase.js
+++ dayflow/js/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-const SUPABASE_URL = 'COLE_SUA_URL_AQUI'
-const SUPABASE_ANON_KEY = 'COLE_SUA_CHAVE_AQUI'
+const SUPABASE_URL = 'https://cvytjwyfiablzaduooya.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2eXRqd3lmaWFibHphZHVvb3lhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ2MTY5MzcsImV4cCI6MjA5MDE5MjkzN30.gcQSjDeoi2_qt_dPhMS0LBjQhIWrMC6zP6HkvJmBmV4'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
